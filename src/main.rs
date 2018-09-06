469f46936b563fdffbc4a3c4aa590b595bfe7114
wegmarken2006 <wegmarken2006@yahoo.it>
2018-09-06 15:58:42 +0200

    mce_channels tested

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ enum Rate {
     RateDr = 3,
 }
 
-
 type UgenList = Vec<Box<Ugen>>;
 
 type RateList = Vec<Rate>;
@@ -27,6 +26,7 @@ struct Control {
     index: i32,
     rate: Rate,
 }
+
 #[derive(Clone, PartialEq, Debug)]
 struct Primitive {
     name: String,
@@ -147,26 +147,26 @@ fn is_sink(ugen: &Ugen) -> bool {
                 }
             }
             ret
-        },
+        }
         Ugen::Mrg(ref mrg) => {
             if is_sink(&*mrg.left) {
                 true
             } else {
                 false
             }
-        },
+        }
         Ugen::Primitive(ref primitive) => {
             if primitive.inputs.len() == 0 {
                 true
             } else {
                 false
             }
-        },
+        }
         _ => false,
     }
 }
 
-fn max_num(nums: Vec<i32>, start: i32 ) -> i32 {
+fn max_num(nums: Vec<i32>, start: i32) -> i32 {
     let mut max = start;
     for elem in nums {
         if elem > max {
@@ -176,10 +176,10 @@ fn max_num(nums: Vec<i32>, start: i32 ) -> i32 {
     max
 }
 
-fn max_rate(rates: RateList, start: Rate ) -> Rate {
+fn max_rate(rates: RateList, start: Rate) -> Rate {
     let mut max = start;
     for elem in rates {
-        if elem as i32 > max as i32{
+        if elem as i32 > max as i32 {
             max = elem;
         }
     }
@@ -195,11 +195,11 @@ fn rate_of(ugen: &Ugen) -> Rate {
                 rates.push(rate_of(&*ugen));
             }
             max_rate(rates, Rate::RateKr)
-        },        
+        }
         Ugen::Mrg(mrg) => rate_of(&*mrg.left),
         Ugen::Primitive(primitive) => primitive.rate,
         Ugen::Proxy(proxy) => proxy.primitive.rate,
-        _ => Rate::RateKr
+        _ => Rate::RateKr,
     }
 }
 
@@ -207,7 +207,7 @@ fn mce_degree(ugen: &Ugen) -> i32 {
     match ugen {
         Ugen::Mce(mce) => mce.ugens.len() as i32,
         Ugen::Mrg(mrg) => mce_degree(&mrg.left),
-        _ => panic!("mce_degree")
+        _ => panic!("mce_degree"),
     }
 }
 
@@ -221,36 +221,38 @@ fn mce_extend(n: i32, ugen: &Ugen) -> UgenList {
             }
             let mut out: UgenList = Vec::new();
             out.push(Box::new(ugen.clone()));
-            out.extend_from_slice(&ex[1 .. n as usize]);
-            out            
-        },
+            out.extend_from_slice(&ex[1..n as usize]);
+            out
+        }
         _ => {
             let mut out: UgenList = Vec::new();
-            for ind in 1 .. n {
+            for ind in 1..n {
                 out.push(Box::new(ugen.clone()));
             }
             out.push(Box::new(ugen.clone()));
-            out            
-
+            out
         }
     }
 }
 
 fn is_mce(ugen: &Ugen) -> bool {
-     match ugen {
+    match ugen {
         Ugen::Mce(mce) => true,
-        _ => false
-     }
+        _ => false,
+    }
 }
 
-fn transposer<T>(list: Vec<Vec<T>>) -> Vec<Vec<T>> where T: Clone {
+fn transposer<T>(list: Vec<Vec<T>>) -> Vec<Vec<T>>
+where
+    T: Clone,
+{
     let len1 = list.len();
     let len2 = (list[0]).len();
-    let mut out : Vec<Vec<T>> = Vec::new();
+    let mut out: Vec<Vec<T>> = Vec::new();
 
-    for ind2 in 0.. len2 as usize {
+    for ind2 in 0..len2 as usize {
         let mut out1: Vec<T> = Vec::new();
-        for ind1 in 0.. len1 as usize {
+        for ind1 in 0..len1 as usize {
             let in1 = &list[ind1];
             let in2 = &in1[ind2];
             out1.push((*in2).clone());
@@ -260,11 +262,15 @@ fn transposer<T>(list: Vec<Vec<T>>) -> Vec<Vec<T>> where T: Clone {
     out
 }
 
-
 fn mce_transform(ugen: &Ugen) -> Ugen {
     match ugen {
         Ugen::Primitive(primitive) => {
-            let ins: UgenList = primitive.clone().inputs.into_iter().filter(|x| is_mce(x)).collect();
+            let ins: UgenList = primitive
+                .clone()
+                .inputs
+                .into_iter()
+                .filter(|x| is_mce(x))
+                .collect();
             let mut degs: Vec<i32> = Vec::new();
             for elem in ins {
                 degs.push(mce_degree(&elem));
@@ -283,22 +289,99 @@ fn mce_transform(ugen: &Ugen) -> Ugen {
             let rate = p.rate;
             let outputs = p.outputs;
             for elem in iet {
-                let new_p = Primitive{inputs: elem.clone(), name: name.clone(),
-                outputs:  outputs.clone(), index: index, special: special, rate: rate};
+                let new_p = Primitive {
+                    inputs: elem.clone(),
+                    name: name.clone(),
+                    outputs: outputs.clone(),
+                    index: index,
+                    special: special,
+                    rate: rate,
+                };
                 out.push(Box::new(Ugen::Primitive(new_p)));
             }
-            Ugen::Mce(Mce{ugens: out})
-        },
-        _ => panic!("mce_transform")
+            Ugen::Mce(Mce { ugens: out })
+        }
+        _ => panic!("mce_transform"),
+    }
+}
+
+fn mce_expand(ugen: &Ugen) -> Ugen {
+    match ugen {
+        Ugen::Mce(mce) => {
+            let mut lst: UgenList = Vec::new();
+            for elem in &mce.ugens {
+                lst.push(Box::new(mce_expand(elem)));
+            }
+            Ugen::Mce(Mce { ugens: lst })
+        }
+        Ugen::Mrg(mrg) => {
+            let lst = mce_expand(&*mrg.left);
+            let right = mrg.right.clone();
+            Ugen::Mrg(Mrg {
+                left: Box::new(lst),
+                right: right,
+            })
+        }
+        _ => {
+            fn rec(ugen: &Ugen) -> bool {
+                match ugen {
+                    Ugen::Primitive(primitive) => {
+                        let ins: UgenList = primitive
+                            .clone()
+                            .inputs
+                            .into_iter()
+                            .filter(|x| is_mce(x))
+                            .collect();
+                        ins.len() != 0
+                    }
+                    _ => false,
+                }
+            }
+            if rec(ugen) {
+                mce_expand(&mce_transform(ugen))
+            } else {
+                ugen.clone()
+            }
+        }
+    }
+}
+
+fn mce_channel(n: i32, ugen: &Ugen) -> Ugen {
+    match ugen {
+        Ugen::Mce(mce) => *mce.ugens[n as usize].clone(),
+        _ => panic!("mce_channel"),
+    }
+}
+
+fn mce_channels(ugen: &Ugen) -> UgenList {
+    match ugen {
+        Ugen::Mce(mce) => mce.ugens.clone(),
+        Ugen::Mrg(mrg) => {
+            let lst = mce_channels(&*mrg.left);
+            if lst.len() <= 1 {
+                panic!("mce_channels");
+            }
+            let mrg1 = Ugen::Mrg(Mrg {
+                left: lst[0].clone(),
+                right: mrg.right.clone(),
+            });
+            let mut out: UgenList = vec![Box::new(mrg1)];
+            out.extend_from_slice(&lst[1..]);
+            out
+        }
+        _ => {
+            let out: UgenList = vec![Box::new(ugen.clone())];
+            out
+        }
     }
 }
 
 //utilities
 fn iconst(val: i32) -> Box<Ugen> {
-    Box::new(Ugen::IConst(IConst{value: val}))
+    Box::new(Ugen::IConst(IConst { value: val }))
 }
 
-fn mk_ugenlist(vargs: &[&Ugen]) -> UgenList{
+fn mk_ugenlist(vargs: &[&Ugen]) -> UgenList {
     let mut out: UgenList = Vec::new();
     for elem in vargs {
         out.push(Box::new((*elem).clone()));
@@ -307,9 +390,15 @@ fn mk_ugenlist(vargs: &[&Ugen]) -> UgenList{
 }
 
 fn mk_mce(ugens: UgenList) -> Ugen {
-    Ugen::Mce(Mce{ugens: ugens})
+    Ugen::Mce(Mce { ugens: ugens })
 }
 
+fn get_primitive(ugen: &Ugen) -> Primitive {
+    match ugen {
+        Ugen::Primitive(primitive) => primitive.clone(),
+        _ => panic!("get_primitive"),
+    }
+}
 
 //////
 fn main() {
@@ -322,43 +411,70 @@ fn main() {
     ugens1.push(Box::new(ci1.clone()));
     ugens1.push(Box::new(cf1.clone()));
     let exu1 = extend(&ugens1, 5);
-    let nums = vec![13,23,38,11];
+    let nums = vec![13, 23, 38, 11];
     let p1 = Ugen::Primitive(Primitive {
-        name: "P1".to_string(), inputs: ugens1.clone(),
-        outputs: vec![Rate::RateKr, Rate::RateIr] ,
+        name: "P1".to_string(),
+        inputs: ugens1.clone(),
+        outputs: vec![Rate::RateKr, Rate::RateIr],
         ..Primitive::default()
     });
+    let pp1 = get_primitive(&p1);
     let p2 = Ugen::Primitive(Primitive {
-        name: "P2".to_string(), rate: Rate::RateAr,
+        name: "P2".to_string(),
+        rate: Rate::RateAr,
         ..Primitive::default()
     });
 
-    let mc1 = Ugen::Mce(Mce{ugens: vec![Box::new(p1.clone()), Box::new(p2.clone())]});
+    let mc1 = Ugen::Mce(Mce {
+        ugens: vec![Box::new(p1.clone()), Box::new(p2.clone())],
+    });
     let mc2 = mk_mce(mk_ugenlist(&[&p1, &p2]));
     let mc3 = mk_mce(mk_ugenlist(&[&p1, &p2, &mc1]));
-    let mg1 = Ugen::Mrg(Mrg{left: Box::new(mc1.clone()), right: Box::new(p1.clone())});
-	let ex1 = mce_extend(3, &mg1);
-    let ic1 = vec![vec![iconst(1),iconst(2)], 
-                   vec![iconst(3),iconst(4)], 
-                   vec![iconst(5),iconst(6)]];
+    let mg1 = Ugen::Mrg(Mrg {
+        left: Box::new(mc1.clone()),
+        right: Box::new(p1.clone()),
+    });
+    let ex1 = mce_extend(3, &mg1);
+    let ic1 = vec![
+        vec![iconst(1), iconst(2)],
+        vec![iconst(3), iconst(4)],
+        vec![iconst(5), iconst(6)],
+    ];
     let l2 = transposer(ic1.clone());
 
     let p3 = Ugen::Primitive(Primitive {
-        name: "P3".to_string(), rate: Rate::RateKr,
-        inputs: mk_ugenlist(&[&mc1, &mc3]), outputs: vec![Rate::RateIr],
+        name: "P3".to_string(),
+        rate: Rate::RateKr,
+        inputs: mk_ugenlist(&[&mc1, &mc3]),
+        outputs: vec![Rate::RateIr],
         ..Primitive::default()
     });
     let mc10 = mce_transform(&p3);
     let mc101 = match mc10 {
         Ugen::Mce(mce) => mce,
-        _ => panic!("mce_transform test")
+        _ => panic!("mce_transform test"),
     };
     let pp3 = &mc101.ugens[2];
     let pp31 = match *(*pp3).clone() {
         Ugen::Primitive(prim) => prim,
-        _ =>  panic!("mce_transform test 2")
+        _ => panic!("mce_transform test 2"),
     };
-        
+    let mg3 = Ugen::Mrg(Mrg {
+        left: Box::new(mc1.clone()),
+        right: Box::new(p2.clone()),
+    });
+    let l22 = mce_channels(&mg3);
+    let el10 = &(*l22[0]);
+    let el11 = &(*l22[1]);
+    let el10t = match el10 {
+        Ugen::Mrg(mrg) => mrg,
+        _ => panic!("mce_channel test"),
+    };
+    let el11t = match el11 {
+        Ugen::Primitive(primitive) => primitive,
+        _ => panic!("mce_channel test 2"),
+    };
+
     println!("end");
 }
 
@@ -372,41 +488,67 @@ fn test1() {
     ugens1.push(Box::new(ci1.clone()));
     ugens1.push(Box::new(cf1.clone()));
     let exu1 = extend(&ugens1, 5);
-    let nums = vec![13,23,38,11];
+    let nums = vec![13, 23, 38, 11];
     let p1 = Ugen::Primitive(Primitive {
-        name: "P1".to_string(), inputs: ugens1.clone(),
-        outputs: vec![Rate::RateKr, Rate::RateIr] ,
+        name: "P1".to_string(),
+        inputs: ugens1.clone(),
+        outputs: vec![Rate::RateKr, Rate::RateIr],
         ..Primitive::default()
     });
     let p2 = Ugen::Primitive(Primitive {
-        name: "P2".to_string(), rate: Rate::RateAr,
+        name: "P2".to_string(),
+        rate: Rate::RateAr,
         ..Primitive::default()
     });
 
-    let mc1 = Ugen::Mce(Mce{ugens: vec![Box::new(p1.clone()), Box::new(p2.clone())]});
+    let mc1 = Ugen::Mce(Mce {
+        ugens: vec![Box::new(p1.clone()), Box::new(p2.clone())],
+    });
     let mc2 = mk_mce(mk_ugenlist(&[&p1, &p2]));
     let mc3 = mk_mce(mk_ugenlist(&[&p1, &p2, &mc1]));
-    let mg1 = Ugen::Mrg(Mrg{left: Box::new(mc1.clone()), right: Box::new(p1.clone())});
-	let ex1 = mce_extend(3, &mg1);
-    let ic1 = vec![vec![iconst(1),iconst(2)], 
-                   vec![iconst(3),iconst(4)], 
-                   vec![iconst(5),iconst(6)]];
+    let mg1 = Ugen::Mrg(Mrg {
+        left: Box::new(mc1.clone()),
+        right: Box::new(p1.clone()),
+    });
+    let ex1 = mce_extend(3, &mg1);
+    let ic1 = vec![
+        vec![iconst(1), iconst(2)],
+        vec![iconst(3), iconst(4)],
+        vec![iconst(5), iconst(6)],
+    ];
     let l2 = transposer(ic1.clone());
 
     let p3 = Ugen::Primitive(Primitive {
-        name: "P3".to_string(), rate: Rate::RateKr,
-        inputs: mk_ugenlist(&[&mc1, &mc3]), outputs: vec![Rate::RateIr],
+        name: "P3".to_string(),
+        rate: Rate::RateKr,
+        inputs: mk_ugenlist(&[&mc1, &mc3]),
+        outputs: vec![Rate::RateIr],
         ..Primitive::default()
     });
     let mc10 = mce_transform(&p3);
     let mc101 = match mc10 {
         Ugen::Mce(mce) => mce,
-        _ => panic!("mce_transform test")
+        _ => panic!("mce_transform test"),
     };
     let pp3 = &mc101.ugens[2];
     let pp31 = match *(*pp3).clone() {
         Ugen::Primitive(prim) => prim,
-        _ =>  panic!("mce_transform test 2")
+        _ => panic!("mce_transform test 2"),
+    };
+        let mg3 = Ugen::Mrg(Mrg {
+        left: Box::new(mc1.clone()),
+        right: Box::new(p2.clone()),
+    });
+    let l22 = mce_channels(&mg3);
+    let el10 = &(*l22[0]);
+    let el11 = &(*l22[1]);
+    let el10t = match el10 {
+        Ugen::Mrg(mrg) => mrg,
+        _ => panic!("mce_channel test"),
+    };
+    let el11t = match el11 {
+        Ugen::Primitive(primitive) => primitive,
+        _ => panic!("mce_channel test 2"),
     };
 
     assert_eq!(o1, o2);
