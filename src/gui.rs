50ad8935583dd3fdfef7eaacf734237aabc0a26d
wegmarken2006 <wegmarken2006@yahoo.it>
2018-09-17 16:14:34 +0200

    gui added

---
+++ src/gui.rs
@@ -0,0 +1,59 @@
+
+use iui::prelude::*;
+use iui::controls::{Label, Button, VerticalBox, Group};
+
+use osc::*;
+
+pub fn run_gui() {
+       // Initialize the UI library
+    let ui = UI::init().expect("Couldn't initialize UI library");
+    // Create a window into which controls can be placed
+    let mut win = Window::new(&ui, "Test App", 200, 200, WindowType::NoMenubar);
+    
+    // Create a vertical layout to hold the controls
+    let mut vbox = VerticalBox::new(&ui);
+    vbox.set_padded(&ui, true);
+
+    let mut group_vbox = VerticalBox::new(&ui);
+    let mut group = Group::new(&ui, "Group");
+
+    // Create two buttons to place in the window
+    let mut button = Button::new(&ui, "Start");
+    button.on_clicked(&ui, {
+        let ui = ui.clone();
+        move |btn| {
+            btn.set_text(&ui, "Started!");
+            sc_start();
+        }
+    });
+    let mut button2 = Button::new(&ui, "Stop");
+    button2.on_clicked(&ui, {
+        let ui = ui.clone();
+        move |btn| {
+            btn.set_text(&ui, "Stopped!");
+        }
+    });
+
+    let mut quit_button = Button::new(&ui, "Quit");
+    quit_button.on_clicked(&ui, {
+        let ui = ui.clone();
+        move |_| {
+            ui.quit();
+        }
+    });
+
+    group_vbox.append(&ui, button, LayoutStrategy::Compact);
+    group_vbox.append(&ui, button2, LayoutStrategy::Compact);
+    group_vbox.append(&ui, quit_button, LayoutStrategy::Compact);
+    group.set_child(&ui, group_vbox);
+    vbox.append(&ui, group, LayoutStrategy::Compact);
+
+
+     // Actually put the button in the window
+    win.set_child(&ui, vbox);
+    // Show the window
+    win.show(&ui);
+    // Run the application
+    ui.main();
+
+}
--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,37 @@ mod osc;
 #[macro_use]
 mod ugens;
 mod sc3;
+mod gui;
+//#![allow(dead_code)]
+
+extern crate iui;
 
 use osc::*;
 use ugens::*;
 use sc3::*;
+use gui::*;
 
 
 //////
 use std::f64;
 fn main() {
     println!("start");
+
+    run_gui();
+
+    println!("\nend");
+    
+    /*
     let ug1 = synthdef("anonymous", &sin_osc(440.0, 0.0));
     print_bytes("final", &ug1);
 
     let ug2 = synthdef("anonymous", &sin_osc_m!(440.0, 0.0, rate: Rate::RateIr));
     print_bytes("final macro", &ug2);
-
+*/
     //sc_start();
 
     
-    let xx = mk_unary_operator(0, f64::abs, -2.0);
-    let zz = add(xx.clone(), xx);
-    print_ugen(&zz);
     
-    println!("\nend");
 
 }
 
